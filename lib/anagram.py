b8cae7ad9432fdb02982c3d1e87af3a0892d5b66
Michael Friedman <minorpianokeys@gmail.com>
2023-12-06 22:05:57 -0500

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list_of_words):
+        matches = []
+        for current_word in list_of_words:
+            letter_list = [letter for letter in current_word]
+            if sorted(letter_list) == sorted(self.word):
+                matches.append(current_word)
+            
+        return matches
